9880f16b1e19dba28ee36ff206bcc0b858771612
rrc-F2024WI-cbugtai <150991072+cbugtai@users.noreply.github.com>
2025-01-08 19:38:09 +0000

    feat: adding swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,31 @@
+//external imports
 import express, { Express } from "express";
 //import express from "express"
 //import { Express } from "express"
 
+// internal imports
+import setupSwagger from "../config/swagger";
+
 const app: Express = express();
+
+setupSwagger(app);
 
 app.get("/",(req,res) => {
     res.send("Hello World!");
+})
+
+/**
+ * @openapi
+ * /tasks:
+ *  get:
+ *   summary: Retrieve a lists of tasks
+ *   tags: [Tasks]
+ *   responses:
+ *    200:
+ *     description: A list of tasks
+ */
+app.get("/tasks", (req, res) => {
+    res.send("Retrieved Tasks")
 })
 
 export default app;
