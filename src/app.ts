b6d47880cd3dde4daf044ee0136e03d5ed007af4
rrc-F2024WI-cbugtai <150991072+cbugtai@users.noreply.github.com>
2025-01-08 20:26:31 +0000

    feat: adding health endpoint and test

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import express, { Express } from "express";
 
 // internal imports
 import setupSwagger from "../config/swagger";
+import { timeStamp } from "console";
 
 const app: Express = express();
 
@@ -26,6 +27,15 @@ app.get("/",(req,res) => {
  */
 app.get("/tasks", (req, res) => {
     res.send("Retrieved Tasks")
+})
+
+app.get("/api/v1/health", (req, res) => {
+    res.json({
+        status: "Ok",
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+        version: "1.0.0"
+    })
 })
 
 export default app;
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,16 +1,35 @@
-// app.get("/",(req,res) => {
-//     res.send("Hello World!");
-// })
-
 import request, { Response } from "supertest";
 
 import app from "../src/app";
 
+// app.get("/",(req,res) => {
+//     res.send("Hello World!");
+// })
 describe("GET /", () => {
     it("should return a 200 response with Hello World!", async () => {
         const response: Response = await request(app).get("/");
 
         expect(response.status).toBe(200);
         expect(response.text).toBe("Hello World!");
+    })
+})
+
+// app.get("/api/v1/health", (req, res) => {
+//     res.json({
+//         status: "Ok",
+//         uptime: process.uptime(),
+//         timestamp: new Date().toISOString(),
+//         version: "1.0.0"
+//     })
+// })
+describe("GET api/v1/health", () => {
+    it("should return server health status", async () =>{
+        const response: Response = await request(app).get("/api/v1/health");
+
+        expect(response.status).toBe(200);
+        expect(response.body.status).toBe("Ok");
+        expect(response.body).toHaveProperty("uptime");
+        expect(response.body).toHaveProperty("timestamp");
+        expect(response.body.version).toBe("1.0.0");
     })
 })
